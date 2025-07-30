b02b1c5b064d404fc5745f656c24ae22b617e736
dem2131 <dem2131@ufl.edu>
2025-07-30 18:55:09 -0400

    modified calculator

--- calculator.py
+++ calculator.py
@@ -1,3 +1,4 @@
+import math
 """
 calculator.py
 - Defines functions used to create a simple calculator
@@ -5,7 +6,20 @@ calculator.py
 One function per operation, in order.
 """
 # First example
-def add(a, b): 
-    pass
+def add(a, b):
+    return a + b
+def sub(a, b):
+    return a - b
+def mul(a, b):
+    return a * b
+def div(a, b):
+    if a == 0:
+        raise ZeroDivisionError("Cannot divide by zero")
+    return b / a
+def log(a, b):
+    if a <= 0 or b <= 0 or a == 1:
+        raise ValueError("Invalid arguments")
+    return math.log(b) / math.log(a)
 
-
+def exp(a, b):
+    return a ** b
